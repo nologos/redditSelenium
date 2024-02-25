d6e089346c234eebadde83399ecd85b612d9b98c
nologos <git.nologos@outlook.com>
2024-02-25 23:51:49 +0000

    Refactor browser initialization and action functions
    - Split the `init_FF` function into two separate functions: `init_FF` for browser initialization and `perform_actions` for performing actions on the browser.
    - Modified `login` and `openitem` functions to accept the browser instance as an argument to ensure the same browser instance is used across all functions.

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
+# item constants 
+PROFILEPAGE = "/html/body/div[1]/div[3]/span[1]/a"
+COMMENTSPAGE = "/html/body/div[1]/div[2]/ul/li[2]/a"
+SUBMITTEDPAGE = "/html/body/div[1]/div[2]/ul/li[3]/a"
+UPVOTEDPAGE = "/html/body/div[1]/div[2]/ul/li[4]/a"
+DOWNVOTEDPAGE = "/html/body/div[1]/div[2]/ul/li[5]/a"
+SAVEDPAGE = "/html/body/div[1]/div[2]/ul/li[7]/a"
+
+
 def load_env():
     # Check if the script is being run from a file
     if os.path.isfile(__name__):
@@ -45,7 +54,7 @@ def get_credentials(func):
 
 
 
-def login(INPUT_username,INPUT_password):
+def login(browser, INPUT_username,INPUT_password):
     element = browser.find_element(By.ID, 'login_login-main')
     username = element.find_element(By.NAME, 'user')
     username.clear()
@@ -59,33 +68,19 @@ def login(INPUT_username,INPUT_password):
 
 
 
-def openitem(item):
+def openitem(browser, item):
     element = browser.find_element(By.XPATH, item)
     element.click()
 
-# item constants 
-PROFILEPAGE = "/html/body/div[1]/div[3]/span[1]/a"
-COMMENTSPAGE = "/html/body/div[1]/div[2]/ul/li[2]/a"
-SUBMITTEDPAGE = "/html/body/div[1]/div[2]/ul/li[3]/a"
-UPVOTEDPAGE = "/html/body/div[1]/div[2]/ul/li[4]/a"
-DOWNVOTEDPAGE = "/html/body/div[1]/div[2]/ul/li[5]/a"
-SAVEDPAGE = "/html/body/div[1]/div[2]/ul/li[7]/a"
 
-def get_entry():
-    print("breakpoint 1")
-    entry = {}
-    dd = browser.find_element(By.CLASS_NAME, 'thing')
-    entry["url"] = dd.find_element(By.CLASS_NAME, 'title').get_attribute('href')
-    entry["title"] = dd.find_element(By.CLASS_NAME, 'title').text
-    entry["myComment"] = dd.find_element(By.CLASS_NAME, 'usertext-body').text
-    contextbutton = dd.find_elements(By.CLASS_NAME, 'bylink')[1]
+def get_context(browser, browserInstance):
+    contextbutton = browserInstance.find_elements(By.CLASS_NAME, 'bylink')[1]
     contextbutton.send_keys(Keys.CONTROL + Keys.RETURN)
     browser.switch_to.window(browser.window_handles[-1])
-    print("breakpoint 2")
     time.sleep(1)
     de = browser.find_element(By.CLASS_NAME, 'nestedlisting')
-    entry["context"] = de.text.replace(
-        '\n\n\n\n\n\n\n\n\n\n\n', "" ).replace(
+    output = de.text.replace(
+        '\n\n\n\n\n\n\n\n\n\n\n', "").replace(
         'permalinksaveeditdisable inbox repliesdelete', '').replace(
         'permalinkembedsaveeditdisable inbox repliesdeletereply', '').replace(
         'thoughtpermalinkembedsaveparenteditdisable inbox repliesdeletereply', '').replace(
@@ -93,33 +88,58 @@ def get_entry():
     browser.close()
     browser.switch_to.window(browser.window_handles[0])
     time.sleep(1)
-    print("breakpoint 3")
-    postbutton = dd.find_element(By.CLASS_NAME, 'title')
+    return output
+    
+
+def delete_item(browser, browserInstance):
+    try:
+        deletebutton = browserInstance.find_element(By.CLASS_NAME, 'del-button')
+        deletebutton.click()
+        time.sleep(1)
+        deletebuttonconfirm = browserInstance.find_elements(By.CLASS_NAME, 'yes')[1]
+        deletebuttonconfirm.click()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+    
+
+
+def get_post(browser, browserInstance):
+    postbutton = browserInstance.find_element(By.CLASS_NAME, 'title')
     postbutton.send_keys(Keys.CONTROL + Keys.RETURN)
     time.sleep(1)
     browser.switch_to.window(browser.window_handles[-1])
     time.sleep(1)
-    print("breakpoint 4")
     try:
         aa = browser.find_element(By.CLASS_NAME, 'expando')
-        entry["post"] = aa.find_element(By.CLASS_NAME, 'md').text
+        text = aa.find_element(By.CLASS_NAME, 'md').text
+        browser.close()
+        browser.switch_to.window(browser.window_handles[0])
+        return text
     except Exception as e:
-        # print(e)
-        entry["post"] = "not text"
-    browser.close()
-    print("breakpoint 5")
-    browser.switch_to.window(browser.window_handles[0])
-    deletebutton = dd.find_element(By.CLASS_NAME, 'del-button')
-    deletebutton.click()
-    time.sleep(1)
-    # temp_database.append(entry)
-    print("breakpoint 6")
-    deletebuttonconfirm = dd.find_elements(By.CLASS_NAME, 'yes')[1]
-    deletebuttonconfirm.click()
-    openitem(COMMENTSPAGE)
-    return entry
+        browser.close()
+        browser.switch_to.window(browser.window_handles[0])
+        return "not text"
 
 
+for count, browserInstance in enumerate(browser.find_elements(By.CLASS_NAME, 'thing')):
+    count, get_post(browser, browserInstance)
+
+
+# entry["deleted"] = delete_item(browser, browserInstance)
+def get_comments(browser):
+    pageitems = []
+    for count, browserInstance in enumerate(browser.find_elements(By.CLASS_NAME, 'thing')):
+        print(count)
+        entry = {}
+        entry["url"] = browserInstance.find_element(By.CLASS_NAME, 'title').get_attribute('href')
+        entry["title"] = browserInstance.find_element(By.CLASS_NAME, 'title').text
+        entry["myComment"] = browserInstance.find_element(By.CLASS_NAME, 'usertext-body').text
+        entry["context"] = get_context(browser,browserInstance)
+        entry["post"] = get_post(browser, browserInstance)
+        pageitems.append(entry)
+    return pageitems
 
 
 
@@ -130,25 +150,20 @@ def init_FF():
     browser = webdriver.Firefox(options=options)
     browser.get('https://old.reddit.com')
     time.sleep(5)
-    login(*get_credentials(load_env))
-    time.sleep(5)
-    openitem(PROFILEPAGE)
-    openitem(COMMENTSPAGE)
     return browser
 
 
-# openitem(PROFILEPAGE)
-# openitem(COMMENTSPAGE)
-# openitem(SUBMITTEDPAGE)
-# openitem(UPVOTEDPAGE)
-# openitem(DOWNVOTEDPAGE)
-# openitem(SAVEDPAGE)
+def init_profilePage(browser):
+    login(browser, *get_credentials(load_env))
+    time.sleep(5)
+    openitem(browser, PROFILEPAGE)
+    openitem(browser, COMMENTSPAGE)
+
+
+
 
 
 # ----
-
-browser = init_FF()
-database= json.loads(open("D:/GIT/temp/database.json", "r").read())
 temp_database = json.loads(open("D:/GIT/temp/database.json", "r").read())
 
 len(json.loads(open("D:/GIT/temp/database.json", "r").read()))
@@ -159,33 +174,30 @@ blank = [print(i["myComment"]) for i in temp_database]
 
 
 
-def main():
-    get_comments()
-    get_submitted()
-    get_upvoted()
-    get_downvoted()
+def main(browser):
+    ddd = get_comments(browser)
+    for d in ddd:
+        temp_database.append(d)
+        open("d:/git/temp/tempfile.tmp", "a").write(","+json.dumps(d))
+    
 
-    get_entry()
-
-
-get_comments()
-def get_comments():
-    for i in range(0, 24):
-        openitem(COMMENTSPAGE)
-        print("loop number: ", i)
-        item = get_entry()
-        temp_database.append(item)
-        openitem(COMMENTSPAGE)
-        time.sleep(1)
+    # get_comments()
+    # get_submitted()
+    # get_upvoted()
+    # get_downvoted()
+    # get_commentItem()
 
 
-for i in range(0,4):
-    print("loop number: ", i)
-    item = None
-    item = main()
-    # backup cache
-    open("d:/git/temp/tempfile.tmp", "a").write(","+json.dumps(item))
-    temp_database.append(item)
+browser = init_FF()
+init_profilePage(browser)
+main(browser)
+
+
+
+
+
+
+
 
 
 
@@ -194,9 +206,5 @@ for i in range(0,4):
 # write to file
 open("D:/GIT/temp/database.json", "w").write(json.dumps(temp_database))
 
-
-# cache
+# cache len
 len(open("d:/git/temp/tempfile.tmp", "r").read().split("url"))
-
-
-
